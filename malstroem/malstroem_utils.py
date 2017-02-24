dca164252baf0acde5fc1a6e8212d75acb8c3462
AsgerPetersen <asgerpetersen@gmail.com>
2017-02-24 11:58:50 +0100

    RunMalstroemCommand special case for Windows

--- malstroem/malstroem_utils.py
+++ malstroem/malstroem_utils.py
@@ -79,14 +79,18 @@ class MalstroemUtils:
 
     @staticmethod
     def runMalstroemCommand(command, command_args, progress):
-        popen_commands = [os.path.join(MalstroemUtils.MalstroemScriptPath(), 'malstroem.exe')]
         popen_commands = [MalstroemUtils.MalstroemScript()]
         popen_commands.append(command)
         popen_commands.extend(command_args)
         loglines = ['Malstroem execution console output']
         loglines.append('Executing ' + ' '.join(popen_commands))
         progress.setInfo('Executing ' + ' '.join(popen_commands))
-        #path = "C:\\Program Files\\ConEmu\\ConEmu\\Scripts;C:\\Program Files\\ConEmu;C:\\Program Files\\ConEmu\\ConEmu;C:\\ProgramData\\Oracle\\Java\\javapath;C:\\Program Files (x86)\\Intel\\iCLS Client\\;C:\\Program Files\\Intel\\iCLS Client\\;C:\\WINDOWS\\system32;C:\\WINDOWS;C:\\WINDOWS\\System32\\Wbem;C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\;C:\\Program Files\\Intel\\Intel(R) Management Engine Components\\DAL;C:\\Program Files\\Intel\\Intel(R) Management Engine Components\\IPT;C:\\Program Files (x86)\\Intel\\Intel(R) Management Engine Components\\DAL;C:\\Program Files (x86)\\Intel\\Intel(R) Management Engine Components\\IPT;C:\\Program Files\\Intel\\WiFi\\bin\\;C:\\Program Files\\Common Files\\Intel\\WirelessCommon\\;C:\\Users\\KlavsPihlkjær\\.dnx\\bin;C:\\Program Files\\Microsoft DNX\\Dnvm\\;C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\110\\Tools\\Binn\\;C:\\Program Files (x86)\\Microsoft SQL Server\\120\\Tools\\Binn\\;C:\\Program Files\\Microsoft SQL Server\\120\\Tools\\Binn\\;C:\\Program Files\\Microsoft SQL Server\\120\\DTS\\Binn\\;C:\\Program Files (x86)\\Microsoft SQL Server\\120\\Tools\\Binn\\ManagementStudio\\;C:\\Program Files (x86)\\Microsoft SQL Server\\120\\DTS\\Binn\\;C:\\Program Files (x86)\\Windows Kits\\10\\Windows Performance Toolkit\\;C:\\Program Files\\Git\\cmd;C:\\Program Files (x86)\\VIDIA Corporation\\PhysX\\Common;C:\\Program Files\\Git\\bin;C:\\Program Files\\Eclipse Che\\eclipse-che-4.0.0-RC3\\tools\\jre\\bin;C:\\Program Files\\Docker Toolbox;C:\\Users\\kpc\\AppData\\Roaming\\pm;C:\\Program Files\\odejs\\;C:\\WINDOWS\\system32;C:\\WINDOWS;C:\\WINDOWS\\System32\\Wbem;C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\;C:\\Users\\kpc\\AppData\\Roaming\\pm;C:\\Python27;C:\\Users\\kpc\\AppData\\Local\\Microsoft\\WindowsApps;C:\\OSGeo4W64\\bin"
+
+        shell = False
+        env = {}
+        if system.isWindows():
+            shell = True
+            env = {'SystemRoot': 'C:\\Windows'}
 
         proc = subprocess.Popen(
             popen_commands,
@@ -94,8 +98,9 @@ class MalstroemUtils:
             stdin=open(os.devnull),
             stderr=subprocess.STDOUT,
             universal_newlines=False,
-            cwd = MalstroemUtils.MalstroemScriptPath(),
-            env= {'SystemRoot': 'C:\\Windows'}
+            cwd=MalstroemUtils.MalstroemScriptPath(),
+            env=env,
+            shell=shell
         )
         returncode = proc.wait()
         
